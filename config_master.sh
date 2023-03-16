684790ad36ac0e39b41eb3c14880fc44f2249230
loumbas <loumbas@gmail.com>
2023-03-16 11:20:34 +0200

    Update config_master.sh

--- config_master.sh
+++ config_master.sh
@@ -1,18 +1,21 @@
-#Source https://www.linuxtechi.com/install-kubernetes-on-ubuntu-22-04/
-#Source https://docs.docker.com/engine/install/ubuntu/
 
-#configure hostnames
-sudo hostnamectl set-hostname "k8smaster.example.net"
+#Run on masters Only
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-cert-extra-sans=nsx-k8s-master.example.com --kubernetes-version 1.24.11
 
-#Disable swap
-sudo swapoff -a
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+mkdir -p $HOME/.kube
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#Load Containerd
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+#Install Antrea
+kubectl apply -f https://raw.githubusercontent.com/vmware-tanzu/antrea/master/build/yamls/antrea.yml
+
+
+#Install Helm
+wget https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz
+tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
+#Install and Configure Metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+kubectl apply -f metallb-config.yaml
