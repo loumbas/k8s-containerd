41e4fae25a432bb6c19d526c8f4cff2f28e23c92
loumbas <loumbas@gmail.com>
2023-03-16 12:02:45 +0200

    Update 5_add_nfs_provisioner.sh
    Install NFS Provisioner

--- 5_add_nfs_provisioner.sh
+++ 5_add_nfs_provisioner.sh
@@ -1,3 +1,9 @@
 #Run On master
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=k8s-nfs.example.com --set nfs.path=/nfs/subdir --set storageClass.onDelete=true
+
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --create-namespace \
+  --namespace nfs-provisioner \
+  --set nfs.server=k8s-nfs.example.com \
+  --set nfs.path=/nfs/nsx \
+  --set storageClass.onDelete=true
