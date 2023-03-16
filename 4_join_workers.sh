c05fc6e74afcb01e37df2787375921968e50389e
loumbas <loumbas@gmail.com>
2023-03-16 11:29:59 +0200

    Update 4_join_workers.sh
    Join Hosts with Command from master and get new token if forgotten

--- 4_join_workers.sh
+++ 4_join_workers.sh
@@ -1,3 +1,6 @@
 #run on workers only
-sudo kubeadm join nsx-k8s-master.example.com:6443  --token <token> \
+sudo kubeadm join k8s-master.example.com:6443  --token <token> \
         --discovery-token-ca-cert-hash <ca-cert-hash>
+
+#In case we do not have the tokens run this on master
+sudo kubeadm token create --print-join-command
